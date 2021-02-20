bf227a33a1da0aced69cb1bb11113233ef65a08c
RIKU <rank228130@gmail.com>
2021-02-20 15:07:56 +0900

    by lesson 7 completed

--- lesson7/index.js
+++ lesson7/index.js
@@ -5,9 +5,10 @@
     { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
   ];
 
+  const ul = document.getElementsByTagName("ul");
+
   function createLi() {
     for (let i = 0; i < datas.length; i++) {
-      const ul = document.getElementsByTagName("ul");
       const li = document.createElement("li");
       const img = document.createElement("img");
       const a = document.createElement("a");
@@ -25,6 +26,23 @@
     }
   }
 
+  // 画像を出力する
+  function showImage() {
+    const img = document.createElement("img");
+    img.classList.add("gif");
+    img.setAttribute("src", "loading-circle.gif");
+    const body = document.getElementsByTagName("body");
+    body[0].appendChild(img);
+  }
+
+  // 画像を削除する
+  function deleteImage() {
+    const gif = document.querySelector(".gif");
+    gif.remove();
+  }
+
+  showImage();
+
   const promise = new Promise((resolve) => {
     setTimeout(() => {
       resolve();
@@ -33,5 +51,6 @@
 
   promise.then(() => {
     createLi();
+    deleteImage();
   });
 })();
