0bdfc0089ff3806747c0a8b047c7c080e16a4ad2
RIKU <rank228130@gmail.com>
2021-02-21 19:53:03 +0900

    resolveに引数datasを取るよう変更

--- lesson5/index.js
+++ lesson5/index.js
@@ -1,13 +1,13 @@
 "use strict";
-(() => {
+{
   const datas = [
     { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
     { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
   ];
 
-  function createLi() {
+  function insertLi(datas) {
     for (let i = 0; i < datas.length; i++) {
-      const ul = document.getElementsByTagName("ul");
+      const list = document.getElementById("list");
       const li = document.createElement("li");
       const img = document.createElement("img");
       const a = document.createElement("a");
@@ -21,15 +21,10 @@
       img.after(text);
 
       li.appendChild(a);
-      ul[0].appendChild(li);
+      list.appendChild(li);
     }
   }
 
-  const promise = new Promise((resolve) => {
-    resolve();
-  });
-
-  promise.then(() => {
-    createLi();
-  });
-})();
+  const firstPromise = new Promise((resolve, reject) => resolve(datas));
+  firstPromise.then((datas) => insertLi(datas));
+}
