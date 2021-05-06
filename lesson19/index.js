24650ff70b22d9f20a6c5cd41060665f6a39fa3e
RIKU <rank228130@gmail.com>
2021-05-06 18:08:46 +0900

    [update] scoped all
     他ファイル間での変数の重複エラーを回避するため

--- lesson19/index.js
+++ lesson19/index.js
@@ -1,182 +1,169 @@
 "use strict";
 
-const resourceUrl = "https://jsondata.okiba.me/v1/json/C58qe210426143842";
-const body = document.getElementById("body");
-const wrapper = document.getElementById("js-wrapper");
-const slideList = document.getElementById("js-slideList");
-const prevButton = document.getElementById("js-prev");
-const nextButton = document.getElementById("js-next");
-const navNum = document.getElementById("js-navNum");
-const navDots = document.getElementById("js-navDots");
+{
+  const resourceUrl = "https://jsondata.okiba.me/v1/json/C58qe210426143842";
+  const wrapper = document.getElementById("js-wrapper");
+  const slideList = document.getElementById("js-slideList");
+  const prevButton = document.getElementById("js-prev");
+  const nextButton = document.getElementById("js-next");
+  const navNum = document.getElementById("js-navNum");
+  const navDots = document.getElementById("js-navDots");
+  let fragmentSlide = document.createDocumentFragment();
+  let fragmentNavDots = document.createDocumentFragment();
+  let timeOutId = null;
 
-/**
- * NOTE:fragmentに関しての備考
- * グローバルで参照 + 2箇所（createImagesShow）使用する必要があるので、ここに定義
- * ▶︎ 要素.forEach(関数名)で処理をする場合（90行目、94行目）ここに定義するのが最適解と判断。
- * ※lesson17でのfragmentの定義場所に誤りがありました
- */
-let fragmentSlide = document.createDocumentFragment();
-let fragmentNavDots = document.createDocumentFragment();
-let timeOutId = null;
+  const slides = {
+    list: null,
+    currentNum: 0,
+    updateCurrentNumberOne(number) {
+      this.currentNum = this.currentNum + number;
+      if (this.currentNum === this.list.length) {
+        this.currentNum = 0;
+      } else if (this.currentNum === -1) {
+        this.currentNum = this.list.length - 1;
+      }
+    },
+    updateCurrentNumberDirectly(index) {
+      this.currentNum = index;
+    },
+  };
 
-const slides = {
-  list: null,
-  currentNum: 0,
-  updateCurrentNumberOne(number) {
-    this.currentNum = this.currentNum + number;
-    if (this.currentNum === this.list.length) {
-      this.currentNum = 0;
-    } else if (this.currentNum === -1) {
-      this.currentNum = this.list.length - 1;
+  const timeout = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const request = async () => {
+    const resource = await fetch(resourceUrl);
+    let json = await resource.json();
+    return json;
+  };
+
+  const fetchData = async () => {
+    let res;
+    try {
+      await timeout(3000);
+      res = await request();
+    } catch (error) {
+      res = [];
+    } finally {
+      return res;
     }
-  },
-  updateCurrentNumberDirectly(index) {
-    this.currentNum = index;
-  },
-};
+  };
 
-const timeout = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-const request = async () => {
-  const resource = await fetch(resourceUrl);
-  let json = await resource.json();
-  return json;
-};
-
-const fetchData = async () => {
-  let res;
-  try {
-    await timeout(3000);
-    res = await request();
-  } catch (error) {
-    res = [];
-  } finally {
-    return res;
-  }
-};
-
-const init = async () => {
-  const datas = await fetchData();
-  if (datas.length === 0) {
-    slideList.textContent = "コンテンツがありません";
-    return;
-  }
-  createImagesShow(datas);
-  autoPlay();
-};
-
-init();
-
-const createImages = (image, index) => {
-  const li = document.createElement("li");
-  const img = document.createElement("img");
-  img.classList.add("image");
-  img.setAttribute("src", image.src);
-  img.setAttribute("alt", image.alt);
-  li.classList.add("listItem");
-  li.dataset.index = index;
-  li.appendChild(img);
-
-  if (index === 0) {
-    li.classList.add("active");
-  }
-
-  wrapper.classList.add("show");
-  fragmentSlide.appendChild(li);
-};
-
-const autoPlay = () => {
-  timeOutId = setTimeout(() => {
-    /**
-     * OPTIMIZE:前回、引数をオブジェクトで取る方法(154行目）を用いようとしましたが、
-     * 扱いが難しかったので断念しました。ご了承ください。
-     */
-    const beforeNum = slides.currentNum;
-    slides.updateCurrentNumberOne(1);
-    changeImage(beforeNum, slides, navDots);
+  const init = async () => {
+    const datas = await fetchData();
+    if (datas.length === 0) {
+      slideList.textContent = "コンテンツがありません";
+      return;
+    }
+    createImagesShow(datas);
     autoPlay();
-  }, 3000);
-};
+  };
 
-const createSlideDots = (slide, index) => {
-  const li = document.createElement("li");
-  li.classList.add("nav__dot");
+  init();
 
-  if (slides.currentNum === index && slide.classList.contains("active")) {
-    li.classList.add("current");
-  }
-  fragmentNavDots.appendChild(li);
+  const createImages = (image, index) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.classList.add("image");
+    img.setAttribute("src", image.src);
+    img.setAttribute("alt", image.alt);
+    li.classList.add("listItem");
+    li.dataset.index = index;
+    li.appendChild(img);
 
-  li.addEventListener("click", () => {
+    if (index === 0) {
+      li.classList.add("active");
+    }
+
+    wrapper.classList.add("show");
+    fragmentSlide.appendChild(li);
+  };
+
+  const autoPlay = () => {
+    timeOutId = setTimeout(() => {
+      const beforeNum = slides.currentNum;
+      slides.updateCurrentNumberOne(1);
+      changeImage(beforeNum, slides, navDots);
+      autoPlay();
+    }, 3000);
+  };
+
+  const createSlideDots = (slide, index) => {
+    const li = document.createElement("li");
+    li.classList.add("nav__dot");
+
+    if (slides.currentNum === index && slide.classList.contains("active")) {
+      li.classList.add("current");
+    }
+    fragmentNavDots.appendChild(li);
+
+    li.addEventListener("click", () => {
+      const beforeNum = slides.currentNum;
+      slides.updateCurrentNumberDirectly(index);
+      changeImage(beforeNum, slides, navDots);
+      clearTimeout(timeOutId);
+      autoPlay();
+    });
+  };
+
+  const createImagesShow = (datas) => {
+    const images = datas.data;
+    images.forEach(createImages);
+    slideList.appendChild(fragmentSlide);
+    slides.list = document.querySelectorAll(".listItem");
+
+    slides.list.forEach(createSlideDots);
+    navDots.appendChild(fragmentNavDots);
+
+    if (slides.currentNum === 0) {
+      prevButton.disabled = true;
+    }
+
+    navNum.textContent = `${slides.currentNum + 1}/${slides.list.length}`;
+  };
+
+  const changePrevNextState = (currentNum) => {
+    if (currentNum === slides.list.length - 1) {
+      nextButton.disabled = true;
+    } else if (currentNum === 0) {
+      prevButton.disabled = true;
+      nextButton.disabled = false;
+    } else {
+      nextButton.disabled = false;
+      prevButton.disabled = false;
+    }
+  };
+
+  const changeImage = (beforeNum, slides, navDots) => {
+    const {
+      list,
+      list: { length },
+      currentNum,
+    } = slides;
+    navNum.textContent = `${currentNum + 1}/${length}`;
+    list[beforeNum].classList.remove("active");
+    list[currentNum].classList.add("active");
+
+    navDots.children[beforeNum].classList.remove("current");
+    navDots.children[currentNum].classList.add("current");
+
+    changePrevNextState(currentNum);
+  };
+
+  prevButton.addEventListener("click", () => {
     const beforeNum = slides.currentNum;
-    slides.updateCurrentNumberDirectly(index);
+    slides.updateCurrentNumberOne(-1);
     changeImage(beforeNum, slides, navDots);
     clearTimeout(timeOutId);
     autoPlay();
   });
-};
 
-const createImagesShow = (datas) => {
-  const images = datas.data;
-  images.forEach(createImages);
-  slideList.appendChild(fragmentSlide);
-  slides.list = document.querySelectorAll(".listItem");
-
-  // ドットの初期化処理
-  slides.list.forEach(createSlideDots);
-  navDots.appendChild(fragmentNavDots);
-
-  if (slides.currentNum === 0) {
-    prevButton.disabled = true;
-  }
-
-  navNum.textContent = `${slides.currentNum + 1}/${slides.list.length}`;
-};
-
-const changePrevNextState = (currentNum) => {
-  if (currentNum === slides.list.length - 1) {
-    nextButton.disabled = true;
-  } else if (currentNum === 0) {
-    prevButton.disabled = true;
-    nextButton.disabled = false;
-  } else {
-    nextButton.disabled = false;
-    prevButton.disabled = false;
-  }
-};
-
-const changeImage = (beforeNum, slides, navDots) => {
-  const {
-    list,
-    list: { length },
-    currentNum,
-  } = slides;
-  navNum.textContent = `${currentNum + 1}/${length}`;
-  list[beforeNum].classList.remove("active");
-  list[currentNum].classList.add("active");
-
-  // ドットの変更処理
-  navDots.children[beforeNum].classList.remove("current");
-  navDots.children[currentNum].classList.add("current");
-
-  // 前後ボタンの状態切替
-  changePrevNextState(currentNum);
-};
-
-prevButton.addEventListener("click", () => {
-  const beforeNum = slides.currentNum;
-  slides.updateCurrentNumberOne(-1);
-  changeImage(beforeNum, slides, navDots);
-  clearTimeout(timeOutId);
-  autoPlay();
-});
-
-nextButton.addEventListener("click", () => {
-  const beforeNum = slides.currentNum;
-  slides.updateCurrentNumberOne(1);
-  changeImage(beforeNum, slides, navDots);
-  clearTimeout(timeOutId);
-  autoPlay();
-});
+  nextButton.addEventListener("click", () => {
+    const beforeNum = slides.currentNum;
+    slides.updateCurrentNumberOne(1);
+    changeImage(beforeNum, slides, navDots);
+    clearTimeout(timeOutId);
+    autoPlay();
+  });
+}
