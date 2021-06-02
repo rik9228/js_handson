0767f30741bf52ac8ed55964c18881289c566792
RIKU <rank228130@gmail.com>
2021-06-02 23:26:46 +0900

    [feat] checkbox to be checked when scroll bottom in modal

---
+++ lesson24/js/register.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const closeButton = document.getElementById("js-close");
+const term = document.getElementById("js-term");
+const form = document.getElementById("js-form");
+const modal = document.getElementById("js-modal");
+const checkbox = document.getElementById("js-checkbox");
+const modalWrapper = document.getElementById("js-modalWrapper");
+const clientHeight = modalWrapper.clientHeight;
+const scrollHeight = modalWrapper.scrollHeight;
+
+term.addEventListener("click", () => {
+  modal.classList.add("show");
+});
+
+closeButton.addEventListener("click", () => {
+  modal.classList.remove("show");
+});
+
+modalWrapper.addEventListener("scroll", (e) => {
+  if (scrollHeight - (clientHeight + e.target.scrollTop) === 0) {
+    checkbox.checked = true;
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  checkbox.checked ? null : e.preventDefault();
+});
