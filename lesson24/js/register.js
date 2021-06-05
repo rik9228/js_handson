a81c12671c3f9924bfc5357a3c8e7c667cbfdad5
RIKU <rank228130@gmail.com>
2021-06-06 01:34:06 +0900

    [feat] modalState

--- lesson24/js/register.js
+++ lesson24/js/register.js
@@ -7,8 +7,15 @@ const modal = document.getElementById("js-modal");
 const checkbox = document.getElementById("js-checkbox");
 const modalWrapper = document.getElementById("js-modalWrapper");
 
+const modalState = {
+  clientHeight: 0,
+  scrollHeight: 0,
+};
+
 term.addEventListener("click", () => {
   modal.classList.add("show");
+  modalState.clientHeight = modalWrapper.clientHeight;
+  modalState.scrollHeight = modalWrapper.scrollHeight;
 });
 
 closeButton.addEventListener("click", () => {
@@ -16,9 +23,7 @@ closeButton.addEventListener("click", () => {
 });
 
 modalWrapper.addEventListener("scroll", (e) => {
-  const clientHeight = modalWrapper.clientHeight;
-  const scrollHeight = modalWrapper.scrollHeight;
-  if (scrollHeight - (clientHeight + e.target.scrollTop) === 0) {
+  if (modalState.scrollHeight - (modalState.clientHeight + e.target.scrollTop) === 0) {
     checkbox.checked = true;
   }
 });
