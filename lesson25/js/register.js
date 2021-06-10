e9a7ff9552141424bcb7a1c3e70f44a6a6287582
RIKU <rank228130@gmail.com>
2021-06-10 20:01:00 +0900

    [refactor] validateTerms,errorMessages to be object

--- lesson25/js/register.js
+++ lesson25/js/register.js
@@ -12,7 +12,6 @@ const userNameForm = document.getElementById("js-userName");
 const emailForm = document.getElementById("js-email");
 const passwordForm = document.getElementById("js-password");
 
-
 const insertErrorMessageElement = (id, element, formElement) => {
   element.id = id;
   const formParentElement = formElement.parentNode;
@@ -20,7 +19,7 @@ const insertErrorMessageElement = (id, element, formElement) => {
 };
 
 const initErrorMessageElementShow = ({ userName, email, password }) => {
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < Object.keys(errorMessageElementIds).length; i++) {
     const errorElement = document.createElement("p");
     errorElement.classList.add("errorMessage");
 
@@ -50,7 +49,7 @@ const errorMessageElementIds = {
   password: "js-passwordError",
 };
 
-// エラーメッセージDOMの生成
+// 初期化処理：エラーメッセージDOMの生成
 initErrorMessageElementShow(errorMessageElementIds);
 
 let validFlags = {
@@ -61,13 +60,88 @@ let validFlags = {
 
 const changeDisabledSubmit = () => {
   if (Object.values(validFlags).includes(false)) {
-    checkbox.disabled = true;
     submit.disabled = true;
+    checkbox.disabled = true;
   } else {
     submit.disabled = false;
     checkbox.disabled = false;
   }
 };
+
+// バリデーション条件
+let validateTerms = {
+  maxUserNameCount: 15,
+  emailRegex: new RegExp(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/),
+  passwordRegex: new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/),
+  /**
+   * NOTE：
+   * ・emailRegex
+   * ^[A-Za-z0-9]{1}：先頭の1文字をアルファベット小文字/大文字/数字で許可
+   * [A-Za-z0-9_.-]*：先頭より2文字目以降"@"までをアルファベット小文字/大文字/数字/アンダースコア/ピリオド/ハイフン、0文字以上を許可
+   * "@"：連続してはいけない
+   * @以降からトップレベルドメインまで:アルファベット小文字/大文字/数字/アンダースコア/ピリオド/ハイフンを許可、1文字以上
+   * トップレベルドメイン:アルファベット小文字/大文字/数字、1文字以上を許可
+   *
+   * ・passwordRegex
+   * ”0～9のいずれかを含む”かつ
+   * ”a～zのいずれかを含む”かつ
+   * ”A～Zのいずれかを含む”、
+   * 0～9、a～z、A～Z、ハイフンのいずれかで構成された8文字以上の文字列を許可
+   */
+};
+
+const errorMessages = {
+  userName: "ユーザー名は15文字以下にしてください。",
+  email: "メールアドレスの形式になっていません。",
+  password: "8文字以上の大小の英数字を交ぜたものにしてください。",
+};
+
+userNameForm.addEventListener("blur", (e) => {
+  const userNameErrorMessage = document.getElementById(errorMessageElementIds.userName);
+  const inputValue = Array.from(e.target.value);
+  if (inputValue.length > validateTerms.maxUserNameCount) {
+    validFlags.userName = false;
+    userNameErrorMessage.textContent = errorMessages.userName;
+  } else {
+    validFlags.userName = true;
+    userNameErrorMessage.textContent = "";
+  }
+  changeDisabledSubmit();
+});
+
+emailForm.addEventListener("blur", (e) => {
+  const emailErrorMessage = document.getElementById(errorMessageElementIds.email);
+  const inputValue = e.target.value;
+  if (validateTerms.emailRegex.test(inputValue)) {
+    validFlags.email = true;
+    emailErrorMessage.textContent = "";
+  } else {
+    validFlags.email = false;
+    emailErrorMessage.textContent = errorMessages.email;
+  }
+  changeDisabledSubmit();
+});
+
+passwordForm.addEventListener("blur", (e) => {
+  const passwordErrorMessage = document.getElementById(errorMessageElementIds.password);
+  const inputValue = e.target.value;
+  if (validateTerms.passwordRegex.test(inputValue)) {
+    validFlags.password = true;
+    passwordErrorMessage.textContent = "";
+  } else {
+    validFlags.password = false;
+    passwordErrorMessage.textContent = errorMessages.password;
+  }
+  changeDisabledSubmit();
+});
+
+modalWrapper.addEventListener("scroll", (e) => {
+  if (scrollHeight - (clientHeight + e.target.scrollTop) === 0) {
+    checkbox.disabled = false;
+    checkbox.checked = true;
+    changeDisabledSubmit();
+  }
+});
 
 let clientHeight = modalWrapper.clientHeight;
 let scrollHeight = modalWrapper.scrollHeight;
@@ -83,72 +157,6 @@ term.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   modal.classList.remove("show");
-});
-
-userNameForm.addEventListener("blur", (e) => {
-  const userNameErrorMessage = document.getElementById("js-userNameError");
-  const inputValue = Array.from(e.target.value);
-  if (inputValue.length > 15) {
-    validFlags.userName = false;
-    userNameErrorMessage.textContent = "ユーザー名は15文字以下にしてください。";
-  } else {
-    validFlags.userName = true;
-    userNameErrorMessage.textContent = "";
-  }
-  changeDisabledSubmit();
-});
-
-emailForm.addEventListener("blur", (e) => {
-  const emailErrorMessage = document.getElementById("js-emailError");
-  /**
-   * NOTE：
-   * ^[A-Za-z0-9]{1}：先頭の1文字をアルファベット小文字/大文字/数字で許可
-   * [A-Za-z0-9_.-]*：先頭より2文字目以降"@"までをアルファベット小文字/大文字/数字/アンダースコア/ピリオド/ハイフン、0文字以上を許可
-   * "@"：連続してはいけない
-   * @以降からトップレベルドメインまで:アルファベット小文字/大文字/数字/アンダースコア/ピリオド/ハイフンを許可、1文字以上
-   * トップレベルドメイン:アルファベット小文字/大文字/数字、1文字以上を許可
-   */
-  const emailRegex = new RegExp(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/);
-  const inputValue = e.target.value;
-
-  if (emailRegex.test(inputValue)) {
-    validFlags.email = true;
-    emailErrorMessage.textContent = "";
-  } else {
-    validFlags.email = false;
-    emailErrorMessage.textContent = "メールアドレスの形式になっていません。";
-  }
-  changeDisabledSubmit();
-});
-
-passwordForm.addEventListener("blur", (e) => {
-  const passwordErrorMessage = document.getElementById("js-passwordError");
-  /**
-   * NOTE：
-   * ”0～9のいずれかを含む”かつ
-   * ”a～zのいずれかを含む”かつ
-   * ”A～Zのいずれかを含む”、
-   * 0～9、a～z、A～Z、ハイフンのいずれかで構成された8文字以上の文字列を許可
-   */
-  const passwordRegex = new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/);
-  const inputValue = e.target.value;
-
-  if (passwordRegex.test(inputValue)) {
-    validFlags.password = true;
-    passwordErrorMessage.textContent = "";
-  } else {
-    validFlags.password = false;
-    passwordErrorMessage.textContent = "8文字以上の大小の英数字を交ぜたものにしてください。";
-  }
-  changeDisabledSubmit();
-});
-
-modalWrapper.addEventListener("scroll", (e) => {
-  if (scrollHeight - (clientHeight + e.target.scrollTop) === 0) {
-    checkbox.disabled = false;
-    checkbox.checked = true;
-    changeDisabledSubmit();
-  }
 });
 
 form.addEventListener("submit", (e) => (checkbox.checked ? null : e.preventDefault()));
