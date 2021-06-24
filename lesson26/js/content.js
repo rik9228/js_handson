258702a92edbd882189c81bab3972dcc6bc28994
RIKU <rank228130@gmail.com>
2021-06-25 00:13:55 +0900

    refactor: rename define

--- lesson26/js/content.js
+++ lesson26/js/content.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const logout = document.getElementById("js-logout");
+const logoutButton = document.getElementById("js-logout");
 
 window.addEventListener("load", () => {
   const token = sessionStorage.getItem("token");
