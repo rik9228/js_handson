d448e4e89d0d47dee5760b4f796d845a7dfdcc29
RIKU <rank228130@gmail.com>
2021-06-28 01:39:03 +0900

    feature: index.js

---
+++ lesson27/js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+window.addEventListener("load", () => {
+  if (localStorage.getItem("token", "far0fja*ff]afaawfqrlzkfq@aq9283af")) {
+    location.href = "contents.html";
+  } else {
+    location.href = "login.html";
+  }
+});
